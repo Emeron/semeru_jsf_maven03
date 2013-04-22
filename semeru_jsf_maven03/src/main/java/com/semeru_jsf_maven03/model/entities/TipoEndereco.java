77677935e22388d2b6c947ef836750df3edb8abb
maria <maria@maria-PC>
2013-04-22 13:45:16 -0300

    Adição da Estrutura de Diretorios Web hibernate.cfg

--- semeru_jsf_maven03/src/main/java/com/semeru_jsf_maven03/model/entities/TipoEndereco.java
+++ semeru_jsf_maven03/src/main/java/com/semeru_jsf_maven03/model/entities/TipoEndereco.java
@@ -1,13 +1,83 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.semeru_jsf_maven03.model.entities;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.*;
+import org.hibernate.annotations.ForeignKey;
+
 /**
- *
- * @author maria
+ *https://github.com/leandrocgsi/semeru_jsf_maven/tree/master/semeru_jsf_maven/src/main/java/br/com/semeru/model/entities
+ * @author Emerson | 21/04
  */
-class TipoEndereco {
+
+@Entity
+@Table(name="tipoendereco")
+public class TipoEndereco implements Serializable{
+    
+     private static final long serialVersionUID = 1L;
+     
+     @Id
+     @GeneratedValue
+     @Column(name= "IdTipiEndereco", nullable=false)
+     private Integer idTipoEndereco;
+     @Column(name= "DescricaoTipiEndereco", nullable=false, length=35)
+     private String descricaoTipiEndereco;
+     
+     @OneToMany(mappedBy = "tipoendereco", fetch = FetchType.LAZY)
+     @ForeignKey(name="EnderecoTipoEndereco")
+     private List<Endereco> enderecos;
+     
+     public TipoEndereco(){
+         
+     }
+
+    public String getDescricaoTipiEndereco() {
+        return descricaoTipiEndereco;
+    }
+
+    public void setDescricaoTipiEndereco(String descricaoTipiEndereco) {
+        this.descricaoTipiEndereco = descricaoTipiEndereco;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public Integer getIdTipoEndereco() {
+        return idTipoEndereco;
+    }
+
+    public void setIdTipoEndereco(Integer idTipoEndereco) {
+        this.idTipoEndereco = idTipoEndereco;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TipoEndereco other = (TipoEndereco) obj;
+        if (this.idTipoEndereco != other.idTipoEndereco && (this.idTipoEndereco == null || !this.idTipoEndereco.equals(other.idTipoEndereco))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (this.idTipoEndereco != null ? this.idTipoEndereco.hashCode() : 0);
+        return hash;
+    }
+     
+     
     
 }
